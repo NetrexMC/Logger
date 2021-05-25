2a635a45d7a565d9edd44386f552960fca2f4614
Olybear9 <olybear9@gmail.com>
2021-05-25 14:47:05 -0500

    📝 chore: Add header

--- mod.ts
+++ mod.ts
@@ -1,3 +1,21 @@
+/**
+ *  _   _      _
+ * | \ | |    | |
+ * |  \| | ___| |_ _ __ _____  __
+ * | . ` |/ _ \ __| '__/ _ \ \/ /
+ * | |\  |  __/ |_| | |  __/>  <
+ * |_| \_|\___|\__|_|  \___/_/\_\
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author Netrex Team
+ * @link https://github.com/NetrexMC
+ *
+ * © Netrex 2020 - 2021
+ */
 export { TimedLogger } from "./src/TimedLogger.ts";
 export { GenericLogger } from "./src/GenericLogger.ts";
 export { Logger } from "./src/Logger.ts";
